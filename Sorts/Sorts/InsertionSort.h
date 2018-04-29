c1220d8fd1570b84ff2f4a1a352bb529549b8c4e
Jonathan Raysor <raysor.jonathan@gmail.com>
2018-04-29 15:58:01 -0400

    Insertion Sort Added

---
+++ Sorts/Sorts/InsertionSort.h
@@ -0,0 +1,13 @@
+//
+//  InsertionSort.h
+//  Sorts
+//
+//  Created by Jonathan Raysor on 4/29/18.
+//  Copyright © 2018 Jonathan Raysor. All rights reserved.
+//
+
+#ifndef InsertionSort_h
+#define InsertionSort_h
+
+
+#endif /* InsertionSort_h */
